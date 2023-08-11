997e47c24090e81213450da58a8b8a4f43e373f8
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-08-11 15:44:13 +0100

    created Pinia store

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,14 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useUserStore = defineStore({
+  id: 'user',
+  state: () => ({
+    user: 0
+  }),
+  getters: {
+    
+  },
+  actions: {
+   
   }
-
-  return { count, doubleCount, increment }
 })
