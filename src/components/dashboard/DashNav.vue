0c183278c5eef2c6eee434da8dca860cc0f4e488
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-08-21 16:51:09 +0100

    new commit

--- src/components/dashboard/DashNav.vue
+++ src/components/dashboard/DashNav.vue
@@ -2,25 +2,24 @@
     <div class="dash-nav">
         <nav class="navbar sticky-top bg-body-tertiary">
             <div class="container-fluid">
-                <div class="navbar-brand dash-nav__img">
-                    <img src="~@/assets/images/img_1/favicon.png">
+                <div class=" dash-nav__nav-img">
+                    <img src="~@/assets/images/img_1/favicon.png" class="dash-nav__nav-img--img">
                 </div>
                 <form class="d-flex" role="search">
-                    <li>
-                        <a class="navbar-brand">Learning hub</a>
-                        <a class="navbar-brand">Resources Hub</a>
-                        <a class="navbar-brand">Events</a>
-                        <a class="navbar-brand">Services</a>
-                    </li>
-                    <li></li>
+                    <a class="navbar-brand">Learning hub</a>
+                    <a class="navbar-brand">Resources hub</a>
+                    <a class="navbar-brand">Events</a>
+                    <a class="navbar-brand">Services</a>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+                        <img src="~@/assets/images/img_1/eclipse.jpg" class="dash-nav__dropdown--img">
+                        <a class="nav-link dropdown-toggle dash-nav__dropdown" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                        </a>
                         <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Separated link</a></li>
                         </ul>
                     </li>
                 </form>
@@ -38,5 +37,56 @@
 @import "@/sass/mixin.scss";
 
 
+.dash-nav {
+    // background-color: #04AA6D;
+    padding: 0.5rem 2rem;
+
+    &__nav-img {
+        &--img {
+            width: 2.17119rem; 
+            // height: 2.17119rem;
+        }
+    }
+
+    &__dropdown {
+        &--img {
+            width: 2.17119rem; 
+            height: 2.17119rem;
+        }
+    }
+}
+
+body {margin:0;}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+// li a:hover:not(.active) {
+//   background-color: #111;
+// }
+
+.active {
+  background-color: #04AA6D;
+}
 
 </style>
