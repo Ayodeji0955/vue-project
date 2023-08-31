ce462603a2b0d3e8372dc229135b7e7986cd5c3d
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-08-31 17:17:54 +0100

    added store

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,20 +1,32 @@
+import axios from 'axios';
 import { defineStore } from 'pinia'
 
 
-export const useAuthStore = defineStore({
-  id: 'auth',
+export const useAuthStore = defineStore( "authStore", {
   state: () => ({
-    // user: "",
-    // fullName: "",
-    // emailAddress: "",
-    // inputValue: "",
+    emailAddress: "",
+    fullName: '',
+    passwordInput: "",
   }),
   getters: {
     
   },
   actions: {
-    // onSubmit () {
-    //   console.log('submitted')
-    // }
-  }
+    async submitForm() {
+      try {
+        const response = await axios.post('auth/register', {
+          emailAddress: this.emailAddress,
+          fullName: this.fullName,
+          passwordInput: this.passwordInput,
+        });
+
+        console.log('Response:', response.data);
+        // Perform any necessary actions after a successful response
+
+      } catch (error) {
+        console.error('Error:', error);
+        // Perform any necessary error handling
+      }
+    },
+  },
 })
