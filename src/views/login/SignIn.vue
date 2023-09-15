edd8159d81a5d90042a0452f0a2c7dc95c5947f1
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-09-15 17:00:12 +0100

    Created login api

--- src/views/login/SignIn.vue
+++ src/views/login/SignIn.vue
@@ -68,7 +68,9 @@ import axios from "axios"
 const user = ref();
 
 onMounted(async () => {
-  const data = axios.get
+  const data = await axios.get('auth/login');
+  console.log(data);
+
 })
 
 </script>
