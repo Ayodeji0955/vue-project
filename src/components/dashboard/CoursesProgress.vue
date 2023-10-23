0090fc7430ddd4d667fc752c6c1359d39cc59b25
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-10-23 16:44:32 +0100

    new commit

--- src/components/dashboard/CoursesProgress.vue
+++ src/components/dashboard/CoursesProgress.vue
@@ -15,67 +15,67 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 courses__sub-col">
-                    <div class="card h-100" style="max-width: 18rem;">
+                <div class="col-md-3 courses__sub-col">
+                    <div class="card courses__card-body">
                         <img src="~@/assets/images/card/frame_1.png" alt="Logo" class="navbar__img">
-                    <div class="card-body">
-                        <h5 class="card-title">Understanding your target customers</h5>
-                        <p class="card-text">Venture Nation</p>
-                        <div class="row">
-                            <div class="progress col-md-10" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                                <div class="progress-bar text-bg-warning" style="width: 45%">75%</div>
-                            </div>
-                            <div class="col-md-6">
-                                <p>Your progress</p>
-                            </div>
-                            <div class="col-md-6">
-                                <p>37%</p>
-                                <p>Rate this course</p>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div class="col-md-4 courses__sub-col">
-                    <div class="card h-100" style="max-width: 18rem;">
-                        <img src="~@/assets/images/card/frame_2.png" alt="Logo" class="navbar__img">
-                    <div class="card-body">
-                        <h5 class="card-title">Understanding your target customers</h5>
-                        <p class="card-text">Venture Nation</p>
-                        <div class="row">
-                            <div class="progress col-md-10" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                                <div class="progress-bar text-bg-warning" style="width: 50%">75%</div>
-                            </div>
-                            <div class="col-md-6">
-                                <p>Your progress</p>
-                            </div>
-                            <div class="col-md-6">
-                                <p>37%</p>
-                                <p>Rate this course</p>
+                        <div class="card-body">
+                            <h5 class="card-title">Understanding your target customers</h5>
+                            <p class="card-text">Venture Nation</p>
+                            <div class="row">
+                                <div class="progress col-md-10" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                                    <div class="progress-bar text-bg-warning" style="width: 45%">75%</div>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>Your progress</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>37%</p>
+                                    <p>Rate this course</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     </div>
                 </div>
                 <div class="col-md-3 courses__sub-col">
-                    <div class="card h-100" style="max-width: 18rem;">
-                        <img src="~@/assets/images/card/frame_3.png" alt="Logo" class="navbar__img">
-                    <div class="card-body">
-                        <h5 class="card-title">Understanding your target customers</h5>
-                        <p class="card-text">Venture Nation</p>
-                        <div class="row">
-                            <div class="progress col-md-10" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                                <div class="progress-bar text-bg-warning" style="width: 25%">75%</div>
-                            </div>
-                            <div class="col-md-6">
-                                <p>Your progress</p>
-                            </div>
-                            <div class="col-md-6">
-                                <p>37%</p>
-                                <p>Rate this course</p>
+                    <div class="card courses__card-body">
+                        <img src="~@/assets/images/card/frame_2.png" alt="Logo" class="navbar__img">
+                        <div class="card-body">
+                            <h5 class="card-title">Understanding your target customers</h5>
+                            <p class="card-text">Venture Nation</p>
+                            <div class="row">
+                                <div class="progress col-md-10" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                                    <div class="progress-bar text-bg-warning" style="width: 50%">75%</div>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>Your progress</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>37%</p>
+                                    <p>Rate this course</p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="col-md-3 courses__sub-col">
+                    <div class="card courses__card-body">
+                        <img src="~@/assets/images/card/frame_3.png" alt="Logo" class="navbar__img">
+                        <div class="card-body">
+                            <h5 class="card-title">Understanding your target customers</h5>
+                            <p class="card-text">Venture Nation</p>
+                            <div class="row">
+                                <div class="progress col-md-12" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                                    <div class="progress-bar text-bg-warning" style="width: 25%">75%</div>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>Your progress</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>37%</p>
+                                    <p>Rate this course</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -105,8 +105,15 @@
     }
 
     &__sub-col {
-         padding: 0.8125rem 1.5625rem 1.375rem 1.125rem;
-         width: 30%;
+        padding: 0.8125rem 1.5625rem 1.375rem 1.125rem;
+        width: 30%;
+    }
+    
+    &__card-body {
+        border-radius: 0.3125rem;
+        background: #FFF;
+        box-shadow: 0px 0px 42.13974px -4.21397px rgba(0, 0, 0, 0.06);
+
     }
 }
 
